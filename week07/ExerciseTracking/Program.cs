b875e5c91dc13ec31ed83964af21bf654cf527e3
danitex24 <fanbase.ng@gmail.com>
2026-06-17 00:29:51 +0100

    ExerciseTracking

--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -1,9 +1,19 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
+        List<Activity> activities = new List<Activity>();
+
+        activities.Add(new Running("03 Jun 2026", 30, 4.8));
+        activities.Add(new Cycling("04 Jun 2026", 45, 20.0));
+        activities.Add(new Swimming("05 Jun 2026", 40, 20));
+
+        foreach (Activity activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
     }
 }
